c69acdba64744375954cd78364e3fd46cd034399
Nicholas Martin <nickmartin1ee7@gmail.com>
2023-08-28 21:57:20 -0400

    Configure logging

--- DisableWakeArmedDevices.WorkerService/Program.cs
+++ DisableWakeArmedDevices.WorkerService/Program.cs
@@ -1,10 +1,23 @@
+using System;
+using System.IO;
+
 using DisableWakeArmedDevices.WorkerService;
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Serilog;
+
 using var host = Host.CreateDefaultBuilder(args)
     .UseWindowsService()
+    .UseSerilog(Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File(Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            nameof(DisableWakeArmedDevices),
+            ".log"),
+            rollingInterval: RollingInterval.Day)
+        .CreateLogger())
     .ConfigureServices((hostContext, services) =>
     {
         services.AddHostedService<Worker>();
